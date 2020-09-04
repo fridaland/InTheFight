4da49b0a371ccb966bfcd5ae596937c2e66d6d1d
Frida Casas <fridacasas.fc@gmail.com>
2020-09-04 12:59:59 -0500

    Remove eslint as build fails on this configuration, adjust gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -38,9 +31,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
-    'gatsby-plugin-eslint'
+    }
   ],
 }
