70abd081c17f0a1c7e6febef9ff2496d3a5ffa5d
Ben Doyle <bdoyle@protonmail.ch>
2020-10-02 00:25:36 +0000

    Switch to bash for the Linux dev env

--- shell.nix
+++ shell.nix
@@ -3,9 +3,6 @@
 , nodejs ? pkgs."nodejs-12_x"}:
 
 (pkgs.buildFHSUserEnv {
-  name  = "node-env";
+  name  = "ITF";
   targetPkgs = p: with p; [ autoconf automake libtool zlib nodejs nodePackages.npm ]; # ++ deps;
-  runScript = ''
-    zsh
-  '';
 }).env
