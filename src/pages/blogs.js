ce520ed41fc7b7446f7bc507b299593ed043b330
connerdodge <conner.dodge22@gmail.com>
2020-10-22 10:29:25 -0600

    feat: add Blogs route and page

---
+++ src/pages/blogs.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/templates/layout"
+import Title from "../components/atoms/title"
+
+const Blogs = () => {
+  return (
+    <>
+      <Layout />
+      <Title text="Blogs" />
+    </>
+  )
+}
+
+export default Blogs;
