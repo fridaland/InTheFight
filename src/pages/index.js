a4743e1cd5632d20789363c0245f352c6297e9bd
Wanderson pinto dos santos <wanderson.santos@mazzafc.tech>
2020-10-24 01:00:27 -0300

    make middle section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,11 @@ import Layout from "../components/templates/layout"
 import Title from "../components/atoms/title"
 import Description from "../components/atoms/description"
 import HomePageLogo from "../components/molecules/homePageLogo"
+import MiddleSection from "../components/organisms/MiddleSection"
+
+import homeImageA from "../images/home-images/home-image-a.png"
+import homeImageB from "../images/home-images/home-image-b.png"
+import homeImageC from "../images/home-images/home-image-c.png"
 
 class IndexPage extends Component {
   render() {
@@ -23,6 +28,25 @@ class IndexPage extends Component {
             </div>
           )
         })}
+        <MiddleSection
+          sections={[
+            {
+              image: homeImageA,
+              title: 'Header',
+              text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+            },
+            {
+              image: homeImageB,
+              title: 'Header',
+              text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+            },
+            {
+              image: homeImageC,
+              title: 'Header',
+              text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+            },
+          ]}
+        />
       </>
     )
   }
